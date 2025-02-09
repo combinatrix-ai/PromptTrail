15cfe8433e84c8ff1cf9723a9f932525534f50eb
hmirin <panda.1192@gmail.com>
2025-02-09 16:55:10 +0900

    feat: enhance release workflows and docs
    Add auto-release process and improve documentation completeness
    
    - Add automatic version detection and release process
    - Enhance documentation with runner and debugging details
    - Add weather forecast API example
    - Improve markdown code block extractor
    - Update README and remove outdated roadmap
    
    (This commit message is auto-generated with examples/dogfooding/commit_with_auto_generated_comment.py)

--- examples/dogfooding/release_note_writer.py
+++ examples/dogfooding/release_note_writer.py
@@ -1,8 +1,8 @@
 import os
+from typing import Optional
 
 import click
 import requests
-from IPython import embed
 
 from prompttrail.agent.runners import CommandLineRunner
 from prompttrail.agent.templates._control import LinearTemplate, LoopTemplate
@@ -23,22 +23,50 @@ def get_patch_url(last_tag: str, current_tag: str = "main") -> str:
 @click.command()
 @click.option("--last-tag", required=True, help="The last tag")
 @click.option("--current-tag", default="main", help="The current tag")
-def write_release_note(last_tag, current_tag):
+@click.option("--non-interactive", is_flag=True, help="Disable interactive mode")
+@click.option("--output", default=None, help="Output file")
+def write_release_note(
+    last_tag: str, current_tag: str, non_interactive: bool, output: Optional[str]
+):
     patch_url = get_patch_url(last_tag, current_tag)
     print(patch_url)
     response = requests.get(patch_url).text
 
-    templates = LinearTemplate(
-        [
-            SystemTemplate(
-                "You're given a patch file. Please write the release note based on the changes in the patch file."
-            ),
-            # The patch can contain {{ }} which is a jinja template syntax. We need to disable jinja for this task
-            # And I don't want to pass it as Metadata, as it's too long and mess the terminal
-            UserTemplate(response, disable_jinja=True),
-            LoopTemplate([AssistantTemplate(), UserTemplate(description="Input: ")]),
-        ]
-    )
+    if not response:
+        print("No changes found between the tags")
+        raise click.Abort("No changes found between the tags")
+
+    if non_interactive:
+        templates = LinearTemplate(
+            [
+                SystemTemplate(
+                    """
+                    You're given a patch file. Please write the release note based on the changes in the patch file.
+                    This is automated API call to generate release note. Therefore, only emit the content of the release markdown.
+                    You don't need ```markdown``` code block.
+                    """
+                ),
+                # The patch can contain {{ }} which is a jinja template syntax. We need to disable jinja for this task
+                # And I don't want to pass it as Metadata, as it's too long and mess the terminal
+                # UserTemplate(response, disable_jinja=True),
+                UserTemplate(response),
+                AssistantTemplate(),
+            ]
+        )
+    else:
+        templates = LinearTemplate(
+            [
+                SystemTemplate(
+                    "You're given a patch file. Please write the release note based on the changes in the patch file."
+                ),
+                # The patch can contain {{ }} which is a jinja template syntax. We need to disable jinja for this task
+                # And I don't want to pass it as Metadata, as it's too long and mess the terminal
+                UserTemplate(response, disable_jinja=True),
+                LoopTemplate(
+                    [AssistantTemplate(), UserTemplate(description="Input: ")]
+                ),
+            ]
+        )
 
     model = GoogleModel(
         configuration=GoogleConfig(
@@ -52,11 +80,15 @@ def write_release_note(last_tag, current_tag):
         model=model, template=templates, user_interface=CLIInterface()
     )
 
-    session = runner.run(session=Session())
+    session = runner.run(session=Session(), debug_mode=True)
 
-    print(session.messages[-1].content)
-
-    embed()
+    if non_interactive:
+        release_note = session.messages[-1].content
+        if output:
+            with open(output, "w") as f:
+                f.write(release_note)
+        print("Release note:")
+        print(session.messages[-1].content)
 
 
 if __name__ == "__main__":
